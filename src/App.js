a5616c92eccf31d94dcc051cf4de4c7f4f5faf5c
Samir8104 <rsam2385@gmai.com>
2022-04-15 14:09:33 -0400

    Navbar (dosent work)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-
-import './App.css';
-
+import Image from "react-bootstrap/Image";
+import Figure from "react-bootstrap/Figure";
+import Button from "react-bootstrap/Button";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
+import { Row } from "react-bootstrap";
+import { Col } from "react-bootstrap";
+import { Navbar } from "react-bootstrap";
 function App() {
   return (
-    <div>
-      hi
-        
-    </div>
+    <Navbar className="top-bar mx-auto" expand="lg">
+  <Container>
+  <Navbar.Brand >Navbar with text</Navbar.Brand>
+
+      {/* <Row className="justify-content-center">
+        <Col className="text-center"></Col>
+        <div className="top-bar">
+          <Button variant="danger">Wells Fargo</Button>
+        </div>
+      </Row>
+    </Container> */}
+    </Container>
+</Navbar>
   );
 }
 
