0a9284f725cd1e5b9dd94805217981a7091c42fd
Set-itac8 set-itac8 <Rsam2385@gmail.com>
2022-04-18 17:50:15 -0400

    updated features page

--- src/Componets/Featured.js
+++ src/Componets/Featured.js
@@ -1,46 +1,69 @@
 import React from "react";
-import {Button} from "react-bootstrap";
+import {Button, Card} from "react-bootstrap";
 import Image from 'react-bootstrap/Image'
 import Carousel from 'react-bootstrap/Carousel'
+import "../CSS/Featured.css";
+
+
 const Featured = () => {
     return(
-<Carousel fade>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="https://cdn.mos.cms.futurecdn.net/2WvkBYCfjuGwUtxPYmexUg.jpg"
-      alt="First slide"
-    />
-    <Carousel.Caption>
-      <h3>FREE Call of Duty skin</h3>
-      <p>Redeem now! Claim before 5/1/2022</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="https://cdn-images.win.gg/resize/w/1000/format/webp/type/progressive/fit/cover/path/wp/uploads/2022/01/Protocol_Bundle_1920x1080-2-1024x576.jpg"
-      alt="Second slide"
-    />
 
-    <Carousel.Caption>
-      <h3>FREE Valorant skin</h3>
-      <p>Redeem now! Claim before 5/1/2022</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="https://static01.nyt.com/images/2021/05/27/fashion/24SNEAKERCOLORS-sacai-SUB/24SNEAKERCOLORS-sacai-SUB-videoSixteenByNineJumbo1600.jpg"
-      alt="Third slide"
-    />
+<div className="parent-div">
 
-    <Carousel.Caption>
-      <h3>Exlusive nike shoe</h3>
-      <p>Buy now, limited time offer, before 5/1/2022</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-</Carousel>
+<Card style={{ width: '18rem' }}>
+  {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+  <Carousel fade>
+          
+
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="https://cdn.mos.cms.futurecdn.net/2WvkBYCfjuGwUtxPYmexUg.jpg"
+              alt="First slide"
+            />
+            <Carousel.Caption>
+              <h3 style={{color: "red"}}>FREE Call of Duty skin</h3>
+              <p  style={{color: "yellow"}}>Redeem now! Claim before 5/1/2022</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="https://cdn-images.win.gg/resize/w/1000/format/webp/type/progressive/fit/cover/path/wp/uploads/2022/01/Protocol_Bundle_1920x1080-2-1024x576.jpg"
+              alt="Second slide"
+            />
+              
+            <Carousel.Caption>
+              <h3 style={{color: "red"}}>FREE Valorant skin</h3>
+              <p style={{color: "yellow"}}>Redeem now! Claim before 5/1/2022</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="https://static01.nyt.com/images/2021/05/27/fashion/24SNEAKERCOLORS-sacai-SUB/24SNEAKERCOLORS-sacai-SUB-videoSixteenByNineJumbo1600.jpg"
+              alt="Third slide"
+            />
+      
+            <Carousel.Caption>
+              <h3 style={{color: "red"}}>Exlusive Nike shoe</h3>
+              <p style={{color: "yellow"}}>Buy now, limited time offer, before 5/1/2022</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+  <Card.Body>
+    <Card.Title>Card Title</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Button variant="primary">Go somewhere</Button>
+  </Card.Body>
+</Card>
+
+
+
+</div>
     );
 }
 export default Featured;
--- src/Componets/homepage.js
+++ src/Componets/homepage.js
@@ -10,12 +10,49 @@ const Homepage = () => {
                 </h1>
             </div>
 
+            <div class='some-page-wrapper'>
+  <div class='row'>
+    <div class='column'>
+      <div class='orange-column'>
+        Some Text in Column One
+      </div>
+    </div>
+    <div class='column'>
+      <div class='blue-column'>
+        Some Text in Column Two
+      </div>
+    </div>
+    <div class='column'>
+      <div class='green-column'>
+        Some Text in Column Three
+      </div>
+    </div>
+  </div>
+  <div class='row 2'>
+    <div class='column'>
+      <div class='green-column'>
+        Some Text in Row 2, Column One
+      </div>
+    </div>
+    <div class='column'>
+      <div class='orange-column'>
+        Some Text in Row 2, Column Two
+      </div>
+    </div>
+    <div class='column'>
+      <div class='blue-column'>
+        Some Text in Row2, Column Three
+      </div>
+    </div>
+  </div>
+</div>
+
             {/* put the buttons inside a parent div. Make the div a "flex box" using CSS property "display"
             after tht, mess around with flexbox properties to try and align them the way you want.
             */}
             <div className="FlexBox">
             <Button  variant="danger">Featured Rewards</Button>
-            <Button  variant="danger">Upcoming Rewards</Button>
+c            <Button  variant="danger">Upcoming Rewards</Button>
             <Button  variant="danger">Minigames</Button>
             </div> 
             
