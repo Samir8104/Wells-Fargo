6d72309d30af75d736f7a3d4353b4f5ffb2c708a
Set-itac8 set-itac8 <Rsam2385@gmail.com>
2022-04-19 20:21:35 -0400

    Working buttons + new homepage

--- src/Componets/Featured.js
+++ src/Componets/Featured.js
@@ -1,42 +1,87 @@
 import React from "react";
-import {Button, Card} from "react-bootstrap";
-import Image from 'react-bootstrap/Image'
-import Carousel from 'react-bootstrap/Carousel'
+import { Button, Card } from "react-bootstrap";
+import Image from "react-bootstrap/Image";
+import Carousel from "react-bootstrap/Carousel";
 import "../CSS/Featured.css";
-
+import RadiantVal from "../static/CrisisBundle.jpg";
+import Nunca from "../static/NuncaSkin.webp";
+import GreenVal from "../static/GreenVal.webp";
+import Hazmat from "../static/HazmatCOD.webp";
+import COD3 from "../static/COD3.jpg";
+import Nike from "../static/Nike1.webp";
+import NikeAir from "../static/nikeAir.webp";
+import { ReactDOM } from "react";
+import { Link } from "react-router-dom";
 
 const Featured = () => {
-    return(
+  return (
+    <div className="parent-div">
+      <div className="top-bar">
+        <h1 className="header">Your Featured Rewards</h1>
+      </div>
 
-<div className="parent-div">
+      <Card style={{ width: "18rem" }}>
+        {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+        <Carousel fade>
+          <Carousel.Item>
+            <img className="d-block w-100" src={RadiantVal} alt="First slide" />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img className="d-block w-100" src={Nunca} alt="Second slide" />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img className="d-block w-100" src={GreenVal} alt="Third slide" />
+          </Carousel.Item>
+        </Carousel>
+        <Card.Body>
+          <Card.Title>Exlusive VALORANT rewards</Card.Title>
+          <Card.Text>
+            Get a FREE Valorant skin just for signing up. Offer ends at 5/1/2022
+          </Card.Text>
+          <Card.Link href="https://playvalorant.com/en-us/">
+            <Button variant="danger">Redeem now!</Button>
+          </Card.Link>
+        </Card.Body>
+      </Card>
 
-<Card style={{ width: '18rem' }}>
-  {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
-  <Carousel fade>
-          
-
+      <Card style={{ width: "18rem" }}>
+        {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+        <Carousel fade>
           <Carousel.Item>
             <img
               className="d-block w-100"
               src="https://cdn.mos.cms.futurecdn.net/2WvkBYCfjuGwUtxPYmexUg.jpg"
               alt="First slide"
             />
-            <Carousel.Caption>
-              <h3 style={{color: "red"}}>FREE Call of Duty skin</h3>
-              <p  style={{color: "yellow"}}>Redeem now! Claim before 5/1/2022</p>
-            </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="https://cdn-images.win.gg/resize/w/1000/format/webp/type/progressive/fit/cover/path/wp/uploads/2022/01/Protocol_Bundle_1920x1080-2-1024x576.jpg"
-              alt="Second slide"
-            />
-              
-            <Carousel.Caption>
-              <h3 style={{color: "red"}}>FREE Valorant skin</h3>
-              <p style={{color: "yellow"}}>Redeem now! Claim before 5/1/2022</p>
-            </Carousel.Caption>
+            <img className="d-block w-100" src={Hazmat} alt="Second slide" />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img className="d-block w-100" src={COD3} alt="Third slide" />
+          </Carousel.Item>
+        </Carousel>
+        <Card.Body>
+          <Card.Title>Exlusive COD rewards</Card.Title>
+          <Card.Text>
+            Get a FREE Call of Duty skin just for signing up. Offer ends at
+            5/10/2022
+          </Card.Text>
+          <Card.Link href="https://www.callofduty.com/warzone/strategyguide/pre-game-preparation/wz-operators">
+          <Button variant="danger">Redeem now!</Button>
+
+          </Card.Link>
+        </Card.Body>
+      </Card>
+
+      <Card style={{ width: "18rem" }}>
+        {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+        <Carousel fade>
+          <Carousel.Item>
+            <img className="d-block w-100" src={Nike} alt="First slide" />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img className="d-block w-100" src={NikeAir} alt="Second slide" />
           </Carousel.Item>
           <Carousel.Item>
             <img
@@ -44,26 +89,20 @@ const Featured = () => {
               src="https://static01.nyt.com/images/2021/05/27/fashion/24SNEAKERCOLORS-sacai-SUB/24SNEAKERCOLORS-sacai-SUB-videoSixteenByNineJumbo1600.jpg"
               alt="Third slide"
             />
-      
-            <Carousel.Caption>
-              <h3 style={{color: "red"}}>Exlusive Nike shoe</h3>
-              <p style={{color: "yellow"}}>Buy now, limited time offer, before 5/1/2022</p>
-            </Carousel.Caption>
           </Carousel.Item>
         </Carousel>
-  <Card.Body>
-    <Card.Title>Card Title</Card.Title>
-    <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
-  </Card.Body>
-</Card>
-
-
-
-</div>
-    );
-}
+        <Card.Body>
+          <Card.Title>Exlusive NIKE rewards</Card.Title>
+          <Card.Text>
+            Buy exlusive NIKE shoes. These shoes can only be bought from the
+            Wells Fargo App. Offer ends at 5/10/2022
+          </Card.Text>
+          <Card.Link href="https://www.nike.com/">
+            <Button variant="danger">Buy now!</Button>
+          </Card.Link>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+};
 export default Featured;
--- src/Componets/homepage.js
+++ src/Componets/homepage.js
@@ -1,65 +1,52 @@
 import React from "react";
 import "../CSS/Homepage.css";
-import {Button} from "react-bootstrap";
+import HomePage from "../static/TestImg.jpg";
+import { Button } from "react-bootstrap";
+
 const Homepage = () => {
-    return(
-        <div>
-            <div className="top-bar">
-                <h1 className="header">
-                Wells Fargo Getit 
-                </h1>
-            </div>
-
-            <div class='some-page-wrapper'>
-  <div class='row'>
-    <div class='column'>
-      <div class='orange-column'>
-        Some Text in Column One
-      </div>
+  return (
+    <div className="home-body">
+     <br />
+     <br />
+     <br />
+     <br />
+     <br />
+     <br />
+     <br />
+     <br />
+     <br />
+     <br />
+     <br />
+     <br />
+     <br />
+     <br />
+     <br />
+     <br />
+     <br />
+     <br />
+     <br />
+     <br />
+     <br />
+     <br />
+     <br />
+     <br />
+     <br />
+     <br />
+     <br />
+     <br />
+     <br />
+     <br />  
+     <br />
+     <br />
+     <br />
+     <br />
+     <br />
+     <br />
+     <br />   
+      <Button className="behind-button" variant="danger">Featured Rewards</Button>
+      {/* <img className="front-image" src={HomePage} /> */}
     </div>
-    <div class='column'>
-      <div class='blue-column'>
-        Some Text in Column Two
-      </div>
-    </div>
-    <div class='column'>
-      <div class='green-column'>
-        Some Text in Column Three
-      </div>
-    </div>
-  </div>
-  <div class='row 2'>
-    <div class='column'>
-      <div class='green-column'>
-        Some Text in Row 2, Column One
-      </div>
-    </div>
-    <div class='column'>
-      <div class='orange-column'>
-        Some Text in Row 2, Column Two
-      </div>
-    </div>
-    <div class='column'>
-      <div class='blue-column'>
-        Some Text in Row2, Column Three
-      </div>
-    </div>
-  </div>
-</div>
-
-            {/* put the buttons inside a parent div. Make the div a "flex box" using CSS property "display"
-            after tht, mess around with flexbox properties to try and align them the way you want.
-            */}
-            <div className="FlexBox">
-            <Button  variant="danger">Featured Rewards</Button>
-c            <Button  variant="danger">Upcoming Rewards</Button>
-            <Button  variant="danger">Minigames</Button>
-            </div> 
-            
-        </div>
-
-
-    );
+  );
 };
 
 export default Homepage;
